ef18f92b9ccfc5fed45ac22e0d931658f08e8264
Stephen Wool <stevewool7@gmail.com>
2019-03-01 11:45:21 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -29,6 +29,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 
 function removeElementFromBeginningOfArray(array) {
   array.slice(1)
+  return array
 }
 
 function destructivelyRemoveElementFromEndOfArray(array) {
