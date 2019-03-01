9b77e78678d3bc73bd12140505c0f11b7db3c304
Stephen Wool <stevewool7@gmail.com>
2019-03-01 11:45:27 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -34,6 +34,7 @@ function removeElementFromBeginningOfArray(array) {
 
 function destructivelyRemoveElementFromEndOfArray(array) {
   array.pop()
+  return array 
 }
 
 function removeElementFromEndOfArray(array) {
